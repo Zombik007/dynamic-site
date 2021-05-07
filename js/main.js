7660788e6a855e675e23b4ed155389efb2e9dd57
Vitaliy Myts <74954880+Zombik007@users.noreply.github.com>
2021-05-08 00:16:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,38 +31,38 @@ function setBgGreet() {
 
   if (hour === 0 || hour < 5) {
     // Ніч після 00:00 до 06:00
-    document.body.style.backgroundImage = "url('./img/The_Beach_8.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_8.jpg')";
     greeting.textContent = "Доброї Ночі";
   } else if (hour < 6) {
     // Ранок
-    document.body.style.backgroundImage = "url('./img/The_Beach_1.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_1.jpg')";
     greeting.textContent = "Доброго Ранку";
   } else if (hour < 9) {
     // Ранок до полудня
-    document.body.style.backgroundImage = "url('./img/The_Beach_2.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_2.jpg')";
     greeting.textContent = "Доброго Ранку";
   } else if (hour < 12) {
     // Полудень
-    document.body.style.backgroundImage = "url('./img/The_Beach_3.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_3.jpg')";
     greeting.textContent = "Доброго Дня";
   } else if (hour < 14) {
     //   День
-    document.body.style.backgroundImage = "url('./img/The_Beach_4.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_4.jpg')";
     greeting.textContent = "Доброго Дня";
   } else if (hour < 17) {
     // День до вечора
-    document.body.style.backgroundImage = "url('./img/The_Beach_5.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_5.jpg')";
     greeting.textContent = "Доброго Дня";
   } else if (hour < 19) {
     // Вечір
-    document.body.style.backgroundImage = "url('./img/The_Beach_6.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_6.jpg')";
     greeting.textContent = "Доброго Вечора";
   } else if (hour < 21) {
     // Вечір до ночі
-    document.body.style.backgroundImage = "url('./img/The_Beach_7.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_7.jpg')";
     greeting.textContent = "Доброго Вечора";
   } else {
-    document.body.style.backgroundImage = "url('./img/The_Beach_8.jpg')";
+    document.body.style.backgroundImage = "url('/img/The_Beach_8.jpg')";
     greeting.textContent = "Доброї Ночі";
   }
 }
